9c9058d47aace1f8c40f066bfaab554cc4a066ee
Carla <ttbrandao_silva@hotmail.com>
2020-10-21 19:04:39 -0300

    bug fix and visual updates

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 
 function getSizes() {
+    document.getElementById("load").style.display = "none";
     var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
     var element = document.getElementById('navbar');
     var navbarItens = document.getElementById("navbar-itens");
     if (isMobile) {
         element.classList.add("nav-mobi");
         navbarItens.style.display = "none";
+        document.getElementById("navbar-toggle").style.display = "block";
     } else {
         element.classList.add("nav-pc");
         document.getElementById("navbar-toggle").style.display = "none";
+        navbarItens.style.display = "block";
     }
     
     var main = document.getElementById("inicio");
     main.style.height = window.innerHeight - 50+"px";
+    main.style.display = "block";
     var about = document.getElementById("sobre");
     about.style.height = window.innerHeight - 50+"px";
     about.style.display = "none";
